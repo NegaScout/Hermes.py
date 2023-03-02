3dda786872ff90763c22c9c87fd59220c89a27e1
wagneja4 <jenik.wagner@gmail.com>
2023-03-03 00:44:30 +0100

    needs work, returns null and breaks await

--- src/backend/sqlite.py
+++ src/backend/sqlite.py
@@ -1,21 +1,23 @@
 from os.path import isfile
 from sqlite3 import connect
 
+def self_with_commit(func):
+    def inner(*args, **kwargs):
+        func(*args, **kwargs)
+        args[0].db_conn.commit()
+    return inner
+
 def with_commit(func):
     def inner(*args, **kwargs):
         func(*args, **kwargs)
-        commit()
+        args[0].db_conn.commit()
     return inner
 
-@with_commit
-def build():
-    if isfile(BUILD_PATH):
-        scriptexec(SQL_PATH)
+#@with_commit
+#def commit(db_conn):
+#    db_conn.commit()
 
-def commit():
-    db_conn.commit()
-
-def close():
+def close(db_conn):
     db_conn.close()
 
 def scriptexec(cursor, path):
