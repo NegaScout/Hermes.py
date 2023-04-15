f828be81148f7ca7b12fe9e18e22eb21d470f5fb
wagneja4 <jenik.wagner@gmail.com>
2023-04-15 18:52:56 +0200

    created corpus for signal handling

--- src/Hermes/signals.py
+++ src/Hermes/signals.py
@@ -3,9 +3,18 @@ import signal
 def signals_init(self):
 
     def hup_handler(signum, frame):
+        self.logger.info("Recieved HUP signal, reloading configuration...")
         for callback in self.hup_callbacks:
             callback()
-        self.logger.info("Recieved HUP")
+    
+    def term_handler(signum, frame):
+        self.logger.info("Recieved TERM/INT signal, gracefully shutting down...")
+        for callback in self.term_callbacks:
+            callback()
+        exit(0)
 
-    self.hup_callbacks = [self.status_hup_handler]
+    self.hup_callbacks = []
+    self.term_callbacks = []
     signal.signal(signal.SIGHUP, hup_handler)
+    signal.signal(signal.SIGTERM, term_handler)
+    signal.signal(signal.SIGINT, term_handler)
