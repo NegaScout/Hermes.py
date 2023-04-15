a247b20de7014375ac1bec58ae3578a9d216e3d8
wagneja4 <jenik.wagner@gmail.com>
2023-04-15 20:49:26 +0200

    remove prefix

--- src/Hermes/discord.py
+++ src/Hermes/discord.py
@@ -9,7 +9,6 @@ def discord_init(self):
     """
     config_predir = self.config["Discord"]
     self.discord_token_path = config_predir["token_path"]
-    self.prefix = config_predir["prefix"]
     self.cog_prefix = config_predir["cog_prefix"]
     self.guild = None
     self.ready = False
